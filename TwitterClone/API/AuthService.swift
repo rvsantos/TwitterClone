76224679421546b19b03c2169f30da8c6ae6beaa
Rafael V. dos Santos <rvsantos00@gmail.com.br>
2020-12-15 21:51:13 -0300

    refactor: Isolando a API de cadastro de usuario em uma classe

---
+++ TwitterClone/API/AuthService.swift
@@ -0,0 +1,52 @@
+//
+//  AuthService.swift
+//  TwitterClone
+//
+//  Created by Rafael V. dos Santos on 15/12/20.
+//
+
+import UIKit
+import Firebase
+
+
+struct AuthCredentials {
+    let email: String
+    let password: String
+    let fullname: String
+    let username: String
+    let profileImage: UIImage
+}
+
+struct AuthService {
+    
+    static let shared = AuthService()
+    
+    func registerUser(credentials: AuthCredentials, completion: @escaping (Error?, DatabaseReference) -> Void) {
+        
+        Auth.auth().createUser(withEmail: credentials.email, password: credentials.password) { (result, error) in
+            if let error = error {
+                fatalError("Error is \(error.localizedDescription)")
+            }
+            
+            guard let imageData = credentials.profileImage.jpegData(compressionQuality: 0.3) else { return }
+            let filename = NSUUID().uuidString
+            let storageRef = STORAGE_PROFILE_IMAGES.child(filename)
+            
+            storageRef.putData(imageData, metadata: nil) { (metadata, error) in
+                storageRef.downloadURL { (url, error) in
+                    guard let profileImageUrl = url?.absoluteString else { return }
+                    
+                    let values = [
+                        "email": credentials.email,
+                        "fullname": credentials.fullname,
+                        "username": credentials.username,
+                        "profileImageUrl": profileImageUrl
+                    ]
+                    
+                    guard let uid = result?.user.uid else { return }
+                    REF_USERS.child(uid).updateChildValues(values, withCompletionBlock: completion)
+                }
+            }
+        }
+    }
+}
--- TwitterClone/Controller/Authentication/RegistrationController.swift
+++ TwitterClone/Controller/Authentication/RegistrationController.swift
@@ -94,32 +94,10 @@ class RegistrationController: UIViewController {
         guard let fullname = self.tfFullname.text else { return }
         guard let username = self.tfUsername.text else { return }
         
-        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
-            if let error = error {
-                fatalError("Error is \(error.localizedDescription)")
-            }
-            
-            guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
-            let filename = NSUUID().uuidString
-            let storageRef = STORAGE_PROFILE_IMAGES.child(filename)
-            
-            storageRef.putData(imageData, metadata: nil) { (metadata, error) in
-                storageRef.downloadURL { (url, error) in
-                    guard let profileImageUrl = url?.absoluteString else { return }
-                    
-                    let values = [
-                        "email": email,
-                        "fullname": fullname,
-                        "username": username,
-                        "profileImageUrl": profileImageUrl
-                    ]
-                    
-                    guard let uid = result?.user.uid else { return }
-                    REF_USERS.child(uid).updateChildValues(values) { (error, ref) in
-                        print("DEBUG: Successfully updated user information...")
-                    }
-                }
-            }
+        let credentials = AuthCredentials(email: email, password: password, fullname: fullname, username: username, profileImage: profileImage)
+        
+        AuthService.shared.registerUser(credentials: credentials) { (error, ref) in
+            print("DEBUG: chegou aqui sem erro...")
         }
     }
     
